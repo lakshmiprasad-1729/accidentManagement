0b5c4d51e4ba02dd8e1898406099d4d641391f20
lakshmiprasad-1729 <panchagirilakshminarayana@gmail.com>
2025-06-29 23:35:50 +0530

    added user feature

--- src/main/java/com/vsp/accidentManagement/controllers/postRequests.java
+++ src/main/java/com/vsp/accidentManagement/controllers/postRequests.java
@@ -59,6 +59,10 @@ public class postRequests {
 //        return  postservices.updateStatusByaFieldEmployee(status,id);
 //    }
 
+    @PutMapping("/user-posts")
+    public  ResponseEntity<ApiResponse<List<Post>>> getUserPosts(){
+        return postservices.getUsersPost();
+    }
 
 
     @GetMapping("/check-auth")
